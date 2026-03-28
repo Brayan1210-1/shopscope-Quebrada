e22b003e1dea293b51ee3650270ff6bbbff0fee1
Ale <alexisquebrada11@gmail.com>
2026-03-28 16:55:00 +0000

    card fallido

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './index.css'
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div  className='bg-gray-100 justify-center place-items-center max-h-7/12'>
+
+    <p className='bg-red-600'>hi</p>
+
+    <h1> hola </h1>
+
+    </div>
    
-   <div> <p className='bg-red-600'>hi</p></div>
   )
-}
+};
 
 export default App
---
+++ src/desing/atoms/card.tsx
@@ -0,0 +1,15 @@
+
+
+export function cardBody () {
+
+   /* falta todos los props jum*/
+
+      return (
+    <div
+      className=
+        "rounded-lg border bg-(--color-card) text-(--color-card-foreground) shadow-sm"
+  
+    />
+  );
+    
+}
